b7a5709d03afa1b6ffaa9d7e1c21b851127a9904
Ba Thiên Le <bathien@hotmail.be>
2021-03-24 11:00:40 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+Setup file
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Thesis',
+    version='1.0.0',
+    description='Code of the master thesis',
+    author='Ba Thien Le',
+    packages=find_packages()
+)
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Init import
+"""
+
+from .datasets import CytomineDataset
+from .losses import Loss
+from .model import NuClick
+from .metrics import IoU, DiceCoefficient, HausdorffDistance
+from .plot import plot_loss
+from .processing import create_signal, post_process
+from .utils import convert_time
