44197003829226ad738eae98920826bcedd9046c
Ba Thiên Le <bathien@hotmail.be>
2021-04-10 16:40:00 +0200

    Add split for cross-validation

--- src/data/split.py
+++ src/data/split.py
@@ -4,6 +4,7 @@ Split the dataset into train/val/test sets from a Cytomine dataset.
 
 import csv
 import logging
+import numpy as np
 import os
 
 from argparse import ArgumentParser
@@ -26,9 +27,8 @@ def parse_arguments():
         The parsed arguments.
     """
 
-    parser = ArgumentParser(
-        description="Split dataset into train/val/test sets."
-    )
+    desc = "Split dataset into train/val/test sets or folds and test sets."
+    parser = ArgumentParser(description=desc)
 
     parser.add_argument(
         '--host',
@@ -59,6 +59,18 @@ def parse_arguments():
         type=float,
         default=0.8,
         help="The train/test split ratio of the dataset."
+    )
+    parser.add_argument(
+        '--cross',
+        type=bool,
+        default=False,
+        help="Whether to split into folds for a cross-validation."
+    )
+    parser.add_argument(
+        '--cv',
+        type=int,
+        default=5,
+        help="If cross is enabled, the number of folds to split the data."
     )
 
     return parser.parse_args()
@@ -147,8 +159,18 @@ if __name__ == '__main__':
         else:
             test_set.extend(labels)
 
-    val_set = train_set[split_train_val:]
-    train_set = train_set[:split_train_val]
+    # Split either into train/val/test or into folds
+    if args.cross:
+        folds = np.array_split(np.array(train_set), args.cv)
+
+        categories = [f'fold{i+1}' for i in range(args.cv)] + ['test']
+        sets = [fold.tolist() for fold in folds] + [test_set]
+    else:
+        val_set = train_set[split_train_val:]
+        train_set = train_set[:split_train_val]
+
+        categories = ['train', 'val', 'test']
+        sets = [train_set, val_set, test_set]
 
     # Save details about the split into a CSV file
     detail_path = os.path.join(args.path, f'details-{args.project_id}.csv')
@@ -159,12 +181,8 @@ if __name__ == '__main__':
         writer.writeheader()
 
         # Write the information of each set
-        pair = zip(
-            ['train', 'validation', 'test'],
-            [train_set, val_set, test_set]
-        )
-        for category, s in pair:
-            for filename in s:
+        for category, filenames in zip(categories, sets):
+            for filename in filenames:
                 annotation_id = int(filename[:-4])
                 annotation = id_to_annotations[annotation_id]
                 image = id_to_images[annotation.image]
@@ -180,15 +198,17 @@ if __name__ == '__main__':
 
     # Split the annotations to the corresponding directories
     subdirs = ['images', 'masks', 'inclusions', 'exclusions']
-    sets = ['train', 'val', 'test']
 
     # Create the directories
-    for s in sets:
+    for category in categories:
         for subdir in subdirs:
-            os.makedirs(os.path.join(args.path, s, subdir), exist_ok=True)
+            os.makedirs(
+                os.path.join(args.path, category, subdir),
+                exist_ok=True
+            )
 
     # Move the images and masks to the destination directory
-    for dataset, name in zip([train_set, val_set, test_set], sets):
+    for dataset, name in zip(sets, categories):
         for filename in dataset:
             for subdir in subdirs:
                 os.rename(
